41f679418b49ded28b1e11b30862853f65f87a3e
Trevor Malone <89212222+trevmal@users.noreply.github.com>
2022-10-07 11:48:05 -0500

    Add files via upload

---
+++ EventTest.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using EventApp;
+
+namespace EventAppTests
+{
+    [TestClass]
+    public class EventTests
+    {
+        [TestMethod]
+        //Test discount if lowercase d is passed
+        public void CalculateDiscount_LowerCased()
+        {
+            //Arrange
+            Event birthday = new Event(4, "Crete", 2000);
+            //Act
+            double discount = birthday.doDiscount("d");
+            //Assert
+            Assert.AreEqual(discount, 1800);
+        }
+
+        [TestMethod]
+        //Test discount if uppercase d is passed.
+        public void CalculateDiscount_UpperCaseD()
+        {
+            //Arrange
+            Event graduation = new Event(3, "Lincoln", 2000);
+            //Act
+            double discount = graduation.doDiscount("D");
+            //Assert
+            Assert.AreEqual(discount, 1800);
+        }
+
+        [TestMethod]
+        //Test late fee if lowercase l is passed.
+        public void CalculateLateFee_lowercasel()
+        {
+            //Arrange
+            Event wedding = new Event(2, "Kansas", 2000);
+            //Act
+            double lateFee = wedding.doDiscount("l");
+            //Assert
+            Assert.AreEqual(lateFee, 2200);
+        }
+
+        [TestMethod]
+        //Test late fee if uppercase l is passed.
+        public void CalculateLateFee_UpperCaseL()
+        {
+            //Arrange
+            Event wedding = new Event(1, "Kansas", 2000);
+            //Act
+            double lateFee = wedding.doDiscount("L");
+            //Assert
+            Assert.AreEqual(lateFee, 2200);
+        }
+
+        [TestMethod]
+        //Test late fee if nothing is passed.
+        public void CalculateDiscount_NoInput()
+        {
+            //Arrange
+            Event birthday = new Event(5, "Crete", 2000);
+            //Act
+            double discount = birthday.doDiscount("");
+            //Assert
+            Assert.AreEqual(discount, 2000);
+        }
+    }
+}
